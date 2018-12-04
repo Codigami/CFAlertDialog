2f6cb7c30976f647ed75ed0f72b4d8a2b6084959
Rahul Shettigar <rahulshettigar33@gmail.com>
2018-12-05 00:23:22 +0530

    Refactored to use AndroidX support libraries.

--- cfalertdialog/src/main/java/com/crowdfire/cfalertdialog/views/CFPushButton.java
+++ cfalertdialog/src/main/java/com/crowdfire/cfalertdialog/views/CFPushButton.java
@@ -2,7 +2,7 @@ package com.crowdfire.cfalertdialog.views;
 
 import android.content.Context;
 import android.graphics.Typeface;
-import android.support.v7.widget.AppCompatButton;
+import androidx.appcompat.widget.AppCompatButton;
 import android.util.AttributeSet;
 import android.view.Gravity;
 
--- cfalertdialogdemo/src/main/java/com/crowdfire/cfalertdialogdemo/views/ColorSelectionView.java
+++ cfalertdialogdemo/src/main/java/com/crowdfire/cfalertdialogdemo/views/ColorSelectionView.java
@@ -4,7 +4,7 @@ import android.content.Context;
 import android.graphics.Color;
 import android.graphics.drawable.GradientDrawable;
 import android.os.Build;
-import android.support.annotation.Nullable;
+import androidx.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.LinearLayout;
